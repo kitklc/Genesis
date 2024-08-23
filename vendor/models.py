f08b993f217508cec8eea2e8bc553470e1a710c5
Claude Kitenge <claude.kitenge@gmail.com>
2024-08-23 08:20:49 +0200

    Vendor Dashboard

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User, UserProfile
+from accounts.utils import send_notification
 
 # Create your models here.
 class vendor(models.Model):
@@ -14,3 +15,24 @@ class vendor(models.Model):
     def __str__(self):
         return self.vendor_name
     
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            # update 
+            orig =vendor.objects.get(pk=self.pk)
+            if orig.is_approved != self.is_approved:
+                mail_template = 'accounts/emails/admin_approved_email.html'
+                context = {
+                        'user':self.user,
+                        'is_approved':self.is_approved,
+                        
+                }
+                if self.is_approved == True:
+                     # send a notification email 
+                    mail_subject = 'Félicitations ! Votre société a été approuvé'
+                    send_notification(mail_subject, mail_template, context)
+                else:
+                    # send a notification email 
+                    mail_subject = "Nous sommes désolés ! votre société n'est pas éligible pour publier vos articles sur Genesis."
+                    send_notification(mail_subject, mail_template, context)
+                    
+        return super(vendor, self).save(*args, **kwargs)   # super function is allow to save method Vendor class 
