b3bdf48575d815f62da95e60ae1f1d62addd3dc4
Claude Kitenge <claude.kitenge@gmail.com>
2024-09-14 19:59:11 +0200

    sans API+ Mail

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -159,7 +159,7 @@ EMAIL_PORT = config('EMAIL_PORT',cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD= 'nbgr komu vouv iwsy' 
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Malewa Ya congo <claude.kitenge@gmail.com>'
+DEFAULT_FROM_EMAIL = '>'
 
 GOOGLE_API_KEY = ''
 
